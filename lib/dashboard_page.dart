578b619800ad4ccc4d7d965a7dbf2114ee3942c1
Cheysuno <mehmettalha.irmak@hotmail.com>
2021-11-27 22:23:01 +0300

    Function Called

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -6,8 +6,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
-import 'package:sleek_circular_slider/sleek_circular_slider.dart';
-import 'hex_color.dart';
 import 'main.dart';
 
 class Dashboard extends StatefulWidget {
